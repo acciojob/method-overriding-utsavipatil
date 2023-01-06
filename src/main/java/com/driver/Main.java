76d8b159041593a5fdc57f2b035c2758865d38be
Utsavi Patil <17bee076@nirmauni.ac.in>
2023-01-06 13:04:52 +0530

    changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -8,15 +8,17 @@ public class Main {
     }
 
     public static class B extends A{
-        @Override
+        @Override //Annotations of java
         String meth(){
             return "Method is overridden in Extendend class B";
         }
     }
   public static void main(String[] args){
     B b = new B();
-    b.meth();
+    b.meth(); //Class A's method
+
+    //B b = new A(); --> Child can not hold the parent object
     B b1 = new B();
-    b1.meth();
+    b1.meth(); //Class B's method
   }
 }
